afd231f9f49f401991cb23edc8afa3d17afaf318
Joe Ma <hokyinma@uw.edu>
2016-06-02 11:53:57 -0700

    Finish part of customer class
    waiting for movie class and the implementation of functions in customer
    class

--- Joe-Testing/Joe-Testing/main.cpp
+++ Joe-Testing/Joe-Testing/main.cpp
@@ -6,31 +6,38 @@
 #include <vector>
 using namespace std;
 
-int main() {
-	ifstream idTextFile("data4customers.txt");
-	string str; 
+bool readCustomerData(ifstream &input) {
+	string str;
 	vector<string> vecId;
 	vector<string> vecLastname;
 	vector<string> vecFirstname;
 
-	if (!idTextFile) cout << "error" << endl;
+	if (!input) return false;
 	// Need to add exception to handle the error if not found the file.
 
-	while (getline(idTextFile, str)) {
+	while (getline(input, str)) {
 		string id; string lastname; string firstname;
 		istringstream(str) >> id >> lastname >> firstname;
-		vecId.push_back(id); 
-		vecLastname.push_back(lastname); 
+		vecId.push_back(id);
+		vecLastname.push_back(lastname);
 		vecFirstname.push_back(firstname);
 	}
 
 	for (int i = 0; i < vecId.size(); i++) {
-		cout << vecId[i] << "\t" << 
-			vecLastname[i] << "\t" << 
+		cout << vecId[i] << "\t" <<
+			vecLastname[i] << "\t" <<
 			vecFirstname[i] << endl;
 	}
+
+	return true;
+}
+
+int main() {
+	ifstream idTextFile("data4customers.txt");
+	readCustomerData(idTextFile);
 
 
 	system("pause");
 	return 0;
 }
+
