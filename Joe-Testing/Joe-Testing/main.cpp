d17b6b3abdc1a027789bda76707c54414a91b848
Joe Ma <hokyinma@uw.edu>
2016-06-02 15:15:44 -0700

    Finish the customer hashtable
    hashtable, hash function, addCustomer

--- Joe-Testing/Joe-Testing/main.cpp
+++ Joe-Testing/Joe-Testing/main.cpp
@@ -4,13 +4,41 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include "customer.h"
 using namespace std;
+
+// Structure of hashtable and hashNode for Customer object
+struct CustomerHashNode {
+	Customer customer;
+	CustomerHashNode *next;
+};
+CustomerHashNode *customerHashTable[10];
+////////////////////////////////////////////////////////////
+
+int hashCustomerID(int id) {
+	string strID = to_string(id);
+	return strID[0] - 48;
+}
+
+void addCustomer(Customer &customer) {
+	int index = hashCustomerID(customer.customerID);
+	if (customerHashTable[index] == NULL) {
+		// if no collision
+		CustomerHashNode *head = new CustomerHashNode;
+		head->customer = customer;
+		head->next = NULL;
+		customerHashTable[index] = head;
+	} else {
+		// Solution of collision
+		CustomerHashNode *newHead = new CustomerHashNode;
+		newHead->customer = customer;
+		newHead->next = customerHashTable[index];
+		customerHashTable[index] = newHead;
+	}
+}
 
 bool readCustomerData(ifstream &input) {
 	string str;
-	vector<string> vecId;
-	vector<string> vecLastname;
-	vector<string> vecFirstname;
 
 	if (!input) return false;
 	// Need to add exception to handle the error if not found the file.
@@ -18,17 +46,9 @@ bool readCustomerData(ifstream &input) {
 	while (getline(input, str)) {
 		string id; string lastname; string firstname;
 		istringstream(str) >> id >> lastname >> firstname;
-		vecId.push_back(id);
-		vecLastname.push_back(lastname);
-		vecFirstname.push_back(firstname);
+		Customer customer(stoi(str), firstname, lastname);
+		addCustomer(customer);
 	}
-
-	for (int i = 0; i < vecId.size(); i++) {
-		cout << vecId[i] << "\t" <<
-			vecLastname[i] << "\t" <<
-			vecFirstname[i] << endl;
-	}
-
 	return true;
 }
 
@@ -36,6 +56,16 @@ int main() {
 	ifstream idTextFile("data4customers.txt");
 	readCustomerData(idTextFile);
 
+	CustomerHashNode *nodePtr;
+	for (int i = 0; i < 10; i++) {
+		nodePtr = customerHashTable[i];
+		while (nodePtr != NULL) {
+			cout << nodePtr->customer.customerID << " ";
+			nodePtr = nodePtr->next;
+		}
+		cout << endl;
+	}
+	
 
 	system("pause");
 	return 0;
